1a24db418b513546421ee2d65c4d72afefb9169b
KhalilM94 <khalil.misbah@protonmail.com>
2023-06-16 11:48:02 -0700

    fix description

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,14 @@
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 /* more headers goes there */
-/**
-* Description: Evaluates the sign of integer,
-*/
+#include <stdio.h>
 /* betty style doc for function main goes there */
+/**
+ * main - Short description, positive_or_negative
+ *
+ * Description: Evaluates sign of a function.
+ * Return: 0
+ */
 int main(void)
 {
 	int n;
