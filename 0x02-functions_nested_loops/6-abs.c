809ca986f64a45898037fc79f9a50640ceb15598
KhalilM94 <khalil.misbah@protonmail.com>
2023-06-28 03:23:27 -0700

    _abs func

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+/* more headers goes there */
+#include "main.h"
+/* betty style doc for function main goes there */
+/**
+ * abs - Short description, positive_or_negative
+ * @n : the ASCII position of the char to test
+ * Description: Evaluates sign of a function.
+ * Return: 0
+ */
+
+int  _abs(int n)
+{
+	if (n < 0)
+	{
+		n = -n;
+	}
+	return (n);
+}
