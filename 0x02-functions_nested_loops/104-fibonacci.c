d7db0e7006f555d4d1d1e2902d637277908f0871
KhalilM94 <khalil.misbah@protonmail.com>
2023-07-08 10:21:45 -0700

    split trick to overcome long long usage restriction

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,16 +10,42 @@
 
 int main(void)
 {
-	float fib1 = 1, fib2 = 2, fib;
+	unsigned long fib1 = 1, fib2 = 2, fib;
+	unsigned long fib1_1, fib1_2, fib2_1, fib2_2;
+	unsigned long m, n;
 	int i;
 
-	printf("%.0f, %.0f", fib1, fib2);
-	for (i = 2; i < 98; i++)
+	printf("%lu, %lu", fib1, fib2);
+	for (i = 2; i < 92; i++)
 	{
 		fib = fib1 + fib2;
-		printf(", %.0f", fib);
+		printf(", %lu", fib);
 		fib1 = fib2;
 		fib2 = fib;
+	}
+	fib1_1 = fib1 / 10000000000;
+	fib1_2 = fib1 % 10000000000;
+	fib2_1 = fib2 / 10000000000;
+	fib2_2 = fib2 % 10000000000;
+
+	for (i = 92; i < 98; i++)
+	{
+		m = fib1_1 + fib2_1;
+		n = fib1_2 + fib2_2;
+		if (fib1_2 + fib2_2 > 9999999999)
+		{
+			m += 1;
+			n %= 10000000000;
+		}
+
+		printf("%lu%lu", m, n);
+		if (i != 97)
+			printf(", ");
+
+		fib1_1 = fib2_1;
+		fib1_2 = fib2_2;
+		fib2_1 = m;
+		fib2_2 = n;
 	}
 	printf("\n");
 	return (0);
