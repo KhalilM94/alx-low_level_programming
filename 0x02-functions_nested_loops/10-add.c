be32f835588a4c1361419eb1193719610fee44ad
KhalilM94 <khalil.misbah@protonmail.com>
2023-07-08 07:49:32 -0700

    a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+/* more headers goes there */
+#include "main.h"
+/* betty style doc for function main goes there */
+/**
+ * add - Short description, positive_or_negative
+ * @a : the ASCII position of the char to test
+ * @b : the ASCII position of the char to test
+ * Description: Evaluates sign of a function.
+ * Return: 0
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
